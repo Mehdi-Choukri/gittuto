bf28cf8f8c96dbd6efcfce11f3ef4d1978d81cfb
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2019-11-01 15:14:55 +0100

    ajout fonction de calcul

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 alert('hello world');
+
+
+function calculer()
+{
+    let nb1 =Number(document.getElementById("nb1").value);
+    let nb2 =Number(document.getElementById("nb2").value);
+   document.getElementById("res").value = nb1+nb2
+}
