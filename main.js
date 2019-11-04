cfe63a9920f9abe8a770e30aacac4e6ef92edaaf
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2019-11-04 11:40:01 +0100

    tous les operations fonctionnent

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function calculer()
     let N1 ="";
     let N2="";
     let operation ;
+    let resultat ;
 let i ;
 i=0;
 let j ;
@@ -35,12 +36,13 @@ let j ;
 
     while (i<valeur.length)
     {
-       if(valeur[i]!='+')
+       if(valeur[i]!='+' && valeur[i]!='-' && valeur[i]!='/' && valeur[i]!='x')
        {
         N1+=valeur[i];
        }
-        if(valeur[i]==='+')
+        if(valeur[i]==='+' || valeur[i]==='-' || valeur[i]==='/' || valeur[i]==='x')
         {
+            operation =valeur[i] ;
             for(j=i+1;j<valeur.length;j++)
             {
                 N2+=valeur[j];
@@ -49,10 +51,26 @@ let j ;
         }
         i++;
     }
+    
+
+    
+
+    switch (operation)
+    {
+        case '+' :  resultat=Number(N1)+Number(N2);
+        break ;
+        case '-' : resultat=Number(N1)-Number(N2);
+        break;
+        case 'x' :  resultat=Number(N1)*Number(N2);
+        break ;
+        case '/' : resultat=Number(N1)/Number(N2);
+        break;
+        default : alert('Operation inexistante');
+    }
  
     
     
-    let resultat =Number(N1)+Number(N2);
+   
     document.getElementById("res_screen").value = Number(resultat) ;
 
 
