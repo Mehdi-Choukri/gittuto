a3712c8c280a06088510e5147505240319620801
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2019-11-04 11:08:35 +0100

    operation addition

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 alert('hello world');
 
-
+/*
 function calculer()
 {
     let nb1 =Number(document.getElementById("nb1").value);
     let nb2 =Number(document.getElementById("nb2").value);
    document.getElementById("res").value = nb1+nb2
 }
+*/
 function multiplier()
 {
     let nb1 =Number(document.getElementById("nbr1").value);
@@ -17,4 +18,42 @@ function multiplier()
 function ecrire (N)
 {
     document.getElementById("res_screen").value += N ;
+}
+
+function calculer()
+{
+    let valeur =document.getElementById("res_screen").value ;
+
+    let N1 ="";
+    let N2="";
+    let operation ;
+let i ;
+i=0;
+let j ;
+
+
+
+    while (i<valeur.length)
+    {
+       if(valeur[i]!='+')
+       {
+        N1+=valeur[i];
+       }
+        if(valeur[i]==='+')
+        {
+            for(j=i+1;j<valeur.length;j++)
+            {
+                N2+=valeur[j];
+            }
+            break;
+        }
+        i++;
+    }
+ 
+    
+    
+    let resultat =Number(N1)+Number(N2);
+    document.getElementById("res_screen").value = Number(resultat) ;
+
+
 }
