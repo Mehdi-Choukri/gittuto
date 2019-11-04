90ced73707ae231826bdfcdbbc7bb5e3d748ad32
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2019-11-04 10:36:41 +0100

    interaction des boutons

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ function multiplier()
     let nb2 =Number(document.getElementById("nbr2").value);
    document.getElementById("res1").value = nb1*nb2 ;
 }
+
+function ecrire (N)
+{
+    document.getElementById("res_screen").value += N ;
+}
